da97d3b297b9839423bd300d9c6e74df0d11f4d8
Aram Ebtekar <aebtekar@gs12626.sp.cs.cmu.edu>
2014-11-12 19:00:06 -0500

    almost compiles

--- IMHA.cpp
+++ IMHA.cpp
@@ -6,6 +6,8 @@
 #include <cassert>
 #include <vector>
 #include <map>
+//#include <unordered_map>
+#include <cstdio>
 //#include <chrono>
 using namespace std;
 
@@ -18,12 +20,13 @@ constexpr int GRID_ROWS = 4;
 constexpr int GRID_COLS = 4;
 constexpr int NUM_THREADS = 4;
 constexpr Cost INFINITE = 1000000000;
+condition_variable main_cv;
 
 class StateData {
   public:
     int mask;
     Cost g, h;
-    multimap<Cost,State*>::const_iterator iter;
+    multimap<Cost,State>::const_iterator iter;
     StateData() {
       mask = 0;
       g = INFINITE;
@@ -32,7 +35,7 @@ class StateData {
 };
 
 //ypedef pair<State,StateData> StateEntry;
-getSuccessors(const State& s)
+vector<State> getSuccessors(const State& s)
 {
   vector<State> successors;
   // insert successors
@@ -45,7 +48,7 @@ class Problem {
     State goal;
     double w1, w2;
     vector<multimap<Cost,State> > open;
-    vector<unordered_map<State,StateData> > data;
+    vector<map<State,StateData> > data;
     int num_discovered;
     int num_expanded;
     bool search_done;
@@ -59,10 +62,10 @@ class Problem {
       return pairwiseH(id, s, goal);
     }
 
-    inline Cost f(int id, const StateData& s_data) { return s_data.g + w1*s_data.h; }
+    inline Cost f(int id, StateData& s_data) { return s_data.g + w1*s_data.h; }
 
-    void insert(int id, State& s, const StateData& s_data) {
-      if (s_data.iter != open[id].end())
+    void insert(int id, State& s, StateData& s_data) {
+      if (s_data.iter != open[id].cend())
         open[id].erase(s_data.iter);
       s_data.iter = open[id].insert(pair<Cost,State>(f(id, s_data), s));
     }
@@ -72,11 +75,11 @@ class Problem {
       open.clear(); open.resize(NUM_THREADS);
       data.clear(); data.resize(NUM_THREADS);
       for (int id = 0; id < NUM_THREADS; ++id) {
-        StateData start_data& = data[id][start];
+        open[id].clear();
+        StateData& start_data = data[id][start];
         start_data.g = 0;
         start_data.h = goalH(id, start);
         start_data.iter = open[id].cend();
-        open[id].clear();
         insert(id, start, start_data);
       }
       num_discovered = 1;
@@ -84,19 +87,23 @@ class Problem {
       search_done = false;
     }
 
-    void expand(int id, const State* const s) {
+    void expand(int id, const State& s) {
+      StateData& s_data = data[id][s];
+      assert(!(s_data.mask & MASK_CLOSED));
+      s_data.mask |= MASK_CLOSED;
+
       for (State t : getSuccessors(s)) {
-        StateData& t_data = data[t];
+        StateData& t_data = data[id][t];
         if (t_data.h == -1) {
-          t_data.h = goalH(t);
-          t_data.iter = open.cend();
+          t_data.h = goalH(id, t);
+          t_data.iter = open[id].cend();
           num_discovered++;
         }
 
         //critical section for updating g-value and inserting
         if(t_data.g > s_data.g + 1){
           t_data.g = s_data.g + 1;
-          if (!(t->mask & MASK_CLOSED))
+          if (!(t_data.mask & MASK_CLOSED))
             insert(id, t, t_data);
         }
       }
@@ -107,17 +114,14 @@ class Problem {
         //get a State to expand
         auto it = open[id].cbegin();
         State s = it->second;
-        StateData& s_data = data[id][s];
         open[id].erase(it);
         
-        num_expands++;
-        
-        assert(!(s_data.mask & MASK_CLOSED));
-        s_data.mask |= MASK_CLOSED;
+        num_expanded++;
 
         if (s == goal) {
           search_done = true;
           //send a message to all processes
+          main_cv.notify_one();
           break;
         }
 
@@ -126,14 +130,13 @@ class Problem {
     }
 
     void solveMaze() {
-      Problem problem;
-      problem.init();
+      init();
 
       mutex cv_mutex;
       unique_lock<mutex> cv_lock(cv_mutex);
-      vector<thread> threads(num_threads);
-      for(int i=0; i<num_threads; i++) {
-        threads[i] = thread(astar_thread);
+      vector<thread> threads(NUM_THREADS);
+      for(int id = 0; id < NUM_THREADS; ++id) {
+        threads[id] = thread(astar_thread, id);
       }
       //printf("main thread go to sleep\n");
       main_cv.wait(cv_lock, []{return search_done;});
@@ -142,9 +145,9 @@ class Problem {
         th.join();
       }
     
-      if(goal->g >= INFINITE || !(goal->mask & MASK_CLOSED)) {
-        printf("Queue is empty....failed to find goal!\n");
-      }
+      //if(goal->g >= INFINITE || !(goal->mask & MASK_CLOSED)) {
+      //  printf("Queue is empty....failed to find goal!\n");
+      //}
     }
 };
     
@@ -156,15 +159,22 @@ int main(int argc, char** argv) {
     for(int j=0; j<1; j++) {
       //load map
 
+      Problem problem;
+      problem.w1 = 2;
+      problem.w2 = 2;
+      //problem.start = ;
+      //problem.goal = ;
+
       //run planner
       Clock::time_point t0 = Clock::now();
-      solveMaze();
+      problem.solveMaze();
       Clock::time_point t1 = Clock::now();
+      Cost path_length = problem.data[0][problem.goal].g;
 
       //report stats
       double dt =  chrono::duration<double, chrono::seconds::period>(t1-t0).count();
-      printf("map %d, goal %d: Path Length=%f Visited Nodes=%d Explored Nodes=%d Planning Time=%f\n",i-1,j,path_length,num_discovered,num_expands,dt);
-      fprintf(fout,"%d %f %f %f %d %f\n",NUM_THREADS,w1,w2,dt,num_expands,path_length);
+      printf("map %d, goal %d: Path Length=%f Visited Nodes=%d Explored Nodes=%d Planning Time=%f\n",i-1,j,path_length,problem.num_discovered,problem.num_expanded,dt);
+      fprintf(fout,"%d %f %f %f %d %f\n",NUM_THREADS,problem.w1,problem.w2,dt,problem.num_expanded,path_length);
     }
   }
   fclose(fout);
