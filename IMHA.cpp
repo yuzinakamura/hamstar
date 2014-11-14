733a44707d17b7133461681d2259549b95f71412
Aram Ebtekar <aebtekar@gs12626.sp.cs.cmu.edu>
2014-11-13 20:40:24 -0500

    search algorithm basically works, requires -std=c++11 -pthread

--- IMHA.cpp
+++ IMHA.cpp
@@ -15,15 +15,24 @@ typedef double Cost;
 typedef vector<vector<int> > State;
 typedef chrono::high_resolution_clock Clock;
 
+array<int,4> dx = {1,0,-1,0};
+array<int,4> dy = {0,1,0,-1};
 constexpr int MASK_CLOSED = 1;
-constexpr int GRID_ROWS = 4;
-constexpr int GRID_COLS = 4;
+constexpr int GRID_ROWS = 3;
+constexpr int GRID_COLS = 3;
 constexpr int NUM_THREADS = 4;
 constexpr Cost INFINITE = 1000000000;
 condition_variable main_cv;
 
+void printState(const State& s) {
+  printf("%d %d %d\n%d %d %d\n%d %d %d\n\n",s[0][0],s[0][1],s[0][2],
+                                            s[1][0],s[1][1],s[1][2],
+                                            s[2][0],s[2][1],s[2][2]);
+}
+
 class StateData {
   public:
+    State bp;
     int mask;
     Cost g, h;
     multimap<Cost,State>::const_iterator iter;
@@ -34,11 +43,27 @@ class StateData {
     }
 };
 
-//ypedef pair<State,StateData> StateEntry;
+//typedef pair<State,StateData> StateEntry;
 vector<State> getSuccessors(const State& s)
 {
   vector<State> successors;
-  // insert successors
+  int zeroR, zeroC;
+  for (int i = 0; i < GRID_ROWS; ++i)
+    for (int j = 0; j < GRID_COLS; ++j)
+      if (s[i][j] == 0)
+        zeroR = i, zeroC = j;
+  for (int d = 0; d < 4; ++d)
+  {
+    int r = zeroR + dx[d];
+    int c = zeroC + dy[d];
+    if (0 <= r && r < GRID_ROWS && 0 <= c && c < GRID_COLS)
+    {
+      State succ = s;
+      succ[zeroR][zeroC] = succ[r][c];
+      succ[r][c] = 0;
+      successors.push_back(succ);
+    }
+  }
   return successors;
 }
 
@@ -92,7 +117,7 @@ class Problem {
       assert(!(s_data.mask & MASK_CLOSED));
       s_data.mask |= MASK_CLOSED;
 
-      for (State t : getSuccessors(s)) {
+      for (State& t : getSuccessors(s)) {
         StateData& t_data = data[id][t];
         if (t_data.h == -1) {
           t_data.h = goalH(id, t);
@@ -103,6 +128,7 @@ class Problem {
         //critical section for updating g-value and inserting
         if(t_data.g > s_data.g + 1){
           t_data.g = s_data.g + 1;
+          t_data.bp = s;
           if (!(t_data.mask & MASK_CLOSED))
             insert(id, t, t_data);
         }
@@ -113,10 +139,13 @@ class Problem {
       while(!search_done) {
         //get a State to expand
         auto it = open[id].cbegin();
+        if (it == open[id].cend()) {
+          // search failed
+          search_done = true;
+          break;
+        }
         State s = it->second;
         open[id].erase(it);
-        
-        num_expanded++;
 
         if (s == goal) {
           search_done = true;
@@ -125,21 +154,20 @@ class Problem {
           break;
         }
 
+        num_expanded++;
         expand(id, s);
       }
     }
 
     void solveMaze() {
-      init();
-
       mutex cv_mutex;
       unique_lock<mutex> cv_lock(cv_mutex);
       vector<thread> threads(NUM_THREADS);
       for(int id = 0; id < NUM_THREADS; ++id) {
-        threads[id] = thread(astar_thread, id);
+        threads[id] = thread([=]{ astar_thread(id); });
       }
       //printf("main thread go to sleep\n");
-      main_cv.wait(cv_lock, []{return search_done;});
+      //main_cv.wait(cv_lock, [=]{ return search_done; });
       //printf("main thread awake\n");
       for(thread& th : threads) {
         th.join();
@@ -150,26 +178,32 @@ class Problem {
       //}
     }
 };
-    
 
 int main(int argc, char** argv) {
   FILE* fout = fopen("stats.csv","w");
   //loop over maps
-  for(int i=1; i<argc; i++) {
+  for(int i=0; i<1/*argc*/; i++) {
     for(int j=0; j<1; j++) {
       //load map
 
       Problem problem;
       problem.w1 = 2;
       problem.w2 = 2;
-      //problem.start = ;
-      //problem.goal = ;
+      problem.start = {{8,7,6},{5,4,3},{2,1,0}};
+      problem.goal = {{0,1,2},{3,4,5},{6,7,8}};
 
       //run planner
       Clock::time_point t0 = Clock::now();
+      problem.init();
       problem.solveMaze();
       Clock::time_point t1 = Clock::now();
       Cost path_length = problem.data[0][problem.goal].g;
+
+      //get path computed by thread #0 in reverse order
+      while (problem.goal != problem.start) {
+        problem.goal = problem.data[0][problem.goal].bp;
+        printState(problem.goal);
+      }
 
       //report stats
       double dt =  chrono::duration<double, chrono::seconds::period>(t1-t0).count();
